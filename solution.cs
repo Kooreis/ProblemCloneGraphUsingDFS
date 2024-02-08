872bb1eccfc9d50166b1612099555c1ff08cad2d
Kooreis <kooreis.official@gmail.com>
2024-02-08 10:52:22 -0600

    This update introduces the cloneGraph function. It checks if the node is null and if so, returns it. Otherwise, it creates a new Map to keep track of visited nodes and calls the dfs function on the node.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,5 @@
-class Node {
-    constructor(val, neighbors) {
-        this.val = val === undefined ? 0 : val;
-        this.neighbors = neighbors === undefined ? [] : neighbors;
-    }
+function cloneGraph(node) {
+    if (!node) return node;
+    const map = new Map();
+    return dfs(node);
 }
